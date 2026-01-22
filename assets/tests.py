5d67984040bb558df244a1ae3ef284c710138440
mahsayama <mahsayama@gmail.com>
2026-01-22 18:23:24 +0700

    Upgrade CI/CD to Professional Workflow

--- assets/tests.py
+++ assets/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HealthCheckTests(TestCase):
+    def test_simple_math(self):
+        """
+        Test sederhana buat mastiin sistem test jalan.
+        Nanti lu bisa ganti ini dengan test beneran.
+        """
+        result = 1 + 1
+        self.assertEqual(result, 2)
+        print("\n✅ System Health Check: AMAN")
